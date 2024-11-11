7622771e0e3ff7a3946afe5d77324b41082d7830
Nathasan1410 <nathanael.santoso@ti.ukdw.ac.id>
2024-11-11 14:05:49 +0700

    update 2 cicil dikit banyak error

--- app/src/main/java/com/example/lentera/WelcomeFragment.kt
+++ app/src/main/java/com/example/lentera/WelcomeFragment.kt
@@ -1,21 +1,24 @@
-package com.example.lentera
+package com.example.excercise_fragment_navigation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
+import com.example.lentera.R
 
-class BluetoothFragment {
+
+class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_bluetooth, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
         val startButton = view.findViewById<Button>(R.id.start)
         startButton.setOnClickListener {
 //Code that runs when the button is clicked
             view.findNavController()
-                .navigate(R.id.action_BluetoothFragment_to_messageFragment)
+                .navigate(R.id.action_WelcomeFragment_to_BluetoothFragment)
         }
         return view
     }
