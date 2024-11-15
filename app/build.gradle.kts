4ffea359d1a36597e272e01d995d94e5488b2907
VincenImanuell <vincen.imanuel@ti.ukdw.ac.id>
2024-11-15 09:22:03 +0700

    gradle error solved, added gallery and create fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.safeArgs)
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.android.application")
-    id("com.google.gms.google-services")
+//    id("com.android.application")
+//    id("com.google.gms.google-services")
 
 }
 
